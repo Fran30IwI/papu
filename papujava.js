34410cd63051e1c7af18ea46edfc2afad2958ea2
Franperoni30 <franperoni30@gmail.com>
2024-03-18 17:39:21 -0300

    ez

--- papujava.js
+++ papujava.js
@@ -1,3 +1,7 @@
-let nombre = prompt ("nivel de papu?")
-
-alert (nombre + "que poco papu")
+function accion(){
+    console.log("esta papurri");
+    var accion = document.getElementsByClassName("nav-enlace");
+    for(var i = 0; i < accion.length; i++){
+        accion[i].classList.toggle("desaparece")
+    }
+}
